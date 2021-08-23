094cd703e3cc1fd2a36afe3cff406bbf7ccbf101
Illia Kononenko <ilyanes88@gmail.com>
2021-08-23 12:30:52 +0300

    finish

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,33 +1,45 @@
-import { alert, defaultModules } from '@pnotify/core';
-import '@pnotify/core/dist/PNotify.css';
-import * as PNotifyMobile from '@pnotify/mobile';
-import '@pnotify/mobile/dist/PNotifyMobile.css';
+// import _ from 'lodash';
+// // import { debounce } from 'lodash/debounce';
+// import { alert, defaultModules } from '@pnotify/core';
+// import '@pnotify/core/dist/PNotify.css';
+// import * as PNotifyMobile from '@pnotify/mobile';
+// import '@pnotify/mobile/dist/PNotifyMobile.css';
+// import '@pnotify/core/dist/BrightTheme.css';
 
-defaultModules.set(PNotifyMobile, {});
+// defaultModules.set(PNotifyMobile, {});
 
-alert({
-  text: 'Too many matches found. Please enter a more specific query!',
-});
+// const refs = {
+//   input: document.getElementById('input-id'),
+//   countriesList: document.getElementById('countries'),
+// };
 
-const refs = {
-  input: document.getElementById('input-id'),
-  countriesList: document.getElementById('countries'),
-};
+// const debounceInput = _.debounce(fetchCountries, 500);
 
-refs.input.addEventListener('input', fetchCountries);
+// refs.input.addEventListener('input', debounceInput);
 
-function fetchCountries(e) {
-  // if
-  const query = e.target.value;
-  if (query) {
-    fetch(`https://restcountries.eu/rest/v2/name/${query}`)
-      .then(response => {
-        return response.json();
-      })
-      .then(countries => {
-        const countriesHTML = countries.map(country => `<h6>${country.name}</h6>`).join('');
-        refs.countriesList.insertAdjacentHTML('afterbegin', countriesHTML);
-      })
-      .catch(console.error);
-  }
+// function fetchCountries(searchQuery) {
+//   // if
+//   const query = searchQuery.target.value;
+//   if (query) {
+//     fetch(`https://restcountries.eu/rest/v2/name/${query}`)
+//       .then(response => {
+//         return response.json();
+//       })
+//       .then(countries => {
+//         console.log(countries.length);
+//         const countriesHTML = countries.map(country => `<h6>${country.name}</h6>`).join('');
+//         alert({
+//           type: 'notice',
+//           text: 'Too many matches found. Please enter a more specific query!',
+//         });
+//         refs.countriesList.insertAdjacentHTML('afterbegin', countriesHTML);
+//       })
+//       .catch(console.error);
+//   }
+// }
+export default function fetchCountries(searchQuery) {
+  const url = `https://restcountries.eu/rest/v2/name/${searchQuery}`;
+  return fetch(url)
+    .then(resp => resp.json())
+    .catch(() => errorServerMessage());
 }
--- src/index.js
+++ src/index.js
@@ -1,1 +1,83 @@
 import './sass/main.scss';
+import '@pnotify/core/dist/BrightTheme.css';
+import { notice, error, Stack } from '@pnotify/core';
+
+import fetchCountries from './fetchCountries';
+import countryListMarkup from './templates/countriesList.hbs';
+import countryCardMarkup from './templates/countryCard.hbs';
+
+const debounce = require('lodash.debounce');
+const refs = {
+  input: document.querySelector('.input__text'),
+  renderBox: document.querySelector('.render_box'),
+  debouncedOnInput: debounce(onInput, 1000),
+};
+
+refs.input.addEventListener('input', refs.debouncedOnInput);
+
+function onInput(event) {
+  const country = event.target.value;
+  fetchCountries(country).then(r => console.log(r));
+  fetchCountries(country)
+    .then(r => renderResult(r))
+    .catch(() => errorServerMessage());
+  clearInput();
+}
+
+function renderResult(arr) {
+  if (arr.length === 1) {
+    createCountryCardMarkup(arr);
+  }
+  if (arr.length >= 2 && arr.length < 10) {
+    createCountryListMarkup(arr);
+  }
+  if (arr.length > 10) {
+    noticeMessage();
+  }
+  if (arr.status === 404) {
+    errorMessage();
+  }
+}
+
+function createCountryListMarkup(arr) {
+  refs.renderBox.innerHTML = countryListMarkup(arr);
+}
+
+function createCountryCardMarkup(arr) {
+  refs.renderBox.innerHTML = countryCardMarkup(arr);
+}
+function clearInput() {
+  setTimeout(() => {
+    refs.input.value = '';
+  }, 2000);
+}
+function noticeMessage() {
+  refs.renderBox.innerHTML = '';
+  notice({
+    title: 'Attention',
+    text: 'To many results, try to specify your search.',
+    width: '300px',
+    minHeight: '15px',
+    delay: 2000,
+  });
+}
+function errorMessage() {
+  refs.renderBox.innerHTML = '';
+  error({
+    title: 'Error',
+    text: 'No matchs found!',
+    width: '300px',
+    minHeight: '15px',
+    delay: 2000,
+  });
+}
+function errorServerMessage() {
+  refs.renderBox.innerHTML = '';
+  error({
+    title: 'Error',
+    text: 'Some things goes wrong, try again later',
+    width: '300px',
+    minHeight: '15px',
+    delay: 2000,
+  });
+}
